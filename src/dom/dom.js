a9765481272eed3e519c410f4c884608c77447d0
anandb104 <210169445+anandb104@users.noreply.github.com>
2026-06-20 00:27:26 +0530

    Added function to add task to the dom and added default project and task title to the dom

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -9,6 +9,20 @@ div.style.height=5+"rem";
 div.style.width=30+"rem";
 div.style.fontSize="20px";
 div.style.fontWeight="bold";
-div.style.borderBottom="3px solid red"
+div.style.borderBottom="3px solid black"
 projectscol.appendChild(div);
 }
+export function addtask(taskname){
+    const projectscol=document.getElementById("tasks-col");
+    let div=document.createElement("div");
+    div.textContent=taskname;
+    div.style.display="flex";
+    div.style.justifyContent="center";
+    div.style.alignItems="center";
+    div.style.height=5+"rem";
+    div.style.width=50+"rem";
+    div.style.fontSize="20px";
+    div.style.fontWeight="bold";
+    div.style.borderBottom="3px solid blue";
+    projectscol.appendChild(div);
+    }
--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
 import{createproject,deleteproject,searchproject,renameproject} from "./application/projects.js";
 import{createtask,markascomplete,changepriority} from "./application/tasks.js";
-import{addproject} from "./dom/dom.js"
+import{addproject,addtask} from "./dom/dom.js"
 
 let projectslist=[];
 createproject(projectslist,"Exercise");
 createtask("walking","have to walk man","20-06-2026","high","NA","Exercise",projectslist);
 console.log(projectslist);
 addproject("Exercise");
+addtask("Walking");
 
 
