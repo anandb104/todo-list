65a531c5da6a3c29f3da2a2d1760e4345c675c00
anandb104 <210169445+anandb104@users.noreply.github.com>
2026-06-19 23:32:49 +0530

    Add function to control logic for each task

--- src/application/tasks.js
+++ src/application/tasks.js
@@ -1,9 +1,14 @@
-export function createtask(title,description,duedate,priority,notes,checklist){
-    return{title,
-            description,
-            duedate,
-            priority,
-            notes,
-            checklist,
-    }
+export function createtask(title,description,duedate,priority,notes,projectname,projectslist){
+    let index=projectslist.findIndex(project=>project.name==projectname);
+    let project=projectslist[index];
+    project.todos.push({
+        title,description,duedate,priority,notes,completed:false
+    });
 }
+export function markascomplete(task){
+    task.completed=true;
+}
+export function changepriority(task,newpriority){
+    task.priority=newpriority;
+}
+
--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 import{createproject,deleteproject,searchproject,renameproject} from "./application/projects.js";
-import{createtask} from "./application/tasks.js"
+import{createtask,markascomplete,changepriority} from "./application/tasks.js"
 let projectslist=[];
 createproject(projectslist,"exercise");
-
-
+createtask("walking","have to walk man","20-06-2026","high","NA","exercise",projectslist);
+console.log(projectslist);
 
 
